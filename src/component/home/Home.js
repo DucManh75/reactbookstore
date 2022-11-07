cdf0b8a4ceb08e83847dc9947f887cc8259c8506
DucManh75 <91884985+DucManh75@users.noreply.github.com>
2022-11-07 22:13:39 +0700

    update

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -4,6 +4,11 @@ import laohac from "./img/laohac.jpg"
 import tatden from "./img/tatden.jpg"
 import truyenkieu from "./img/truyenkieu.png"
 import nhatkitrongtu from "./img/nhatkitrongtu.jpg"
+import rungxanu from "./img/rungxanu.jpg"
+import datphuongnam from "./img/datphuongnam.jpg"
+import demenphieuluuky from "./img/demenphieuluuky.jpg"
+import nhungngaythoau from "./img/nhungngaythoau.jpg"
+import lamdi from "./img/lamdi.jpg"
 
 import './Home.css'
 
@@ -86,7 +91,82 @@ function Home() {
                       <div className='product-info'> 
                             <a href='' className='product-name'>Nhật ký trong tù</a>
                             <div className='product-author'>Hồ Chí Minh</div>
-                            <div className='product-price'> 245.000 vnd</div>
+                            <div className='product-price'> 245.000 đ</div>
+                      </div>
+                      <button className='btn' type='submit'> Chọn mua </button>
+                  </div>
+              </li>
+              <li>
+                  <div className='product-item'>
+                      <div className='product-top'>
+                      <a href="" id="truyen">
+                          <img src={rungxanu} alt="Rừng xà nu"/>
+                      </a>
+                      </div>
+                      <div className='product-info'> 
+                            <a href='' className='product-name'>Rừng xà nu</a>
+                            <div className='product-author'>Nguyễn Trung Thành</div>
+                            <div className='product-price'> 65.000 đ</div>
+                      </div>
+                      <button className='btn' type='submit'> Chọn mua </button>
+                  </div>
+              </li>
+              <li>
+                  <div className='product-item'>
+                      <div className='product-top'>
+                      <a href="" id="truyen">
+                          <img src={datphuongnam} alt="Đất rừng phương nam"/>
+                      </a>
+                      </div>
+                      <div className='product-info'> 
+                            <a href='' className='product-name'>Đất rừng phương nam</a>
+                            <div className='product-author'>Đoàn giỏi</div>
+                            <div className='product-price'> 90.000 đ</div>
+                      </div>
+                      <button className='btn' type='submit'> Chọn mua </button>
+                  </div>
+              </li>
+              <li>
+                  <div className='product-item'>
+                      <div className='product-top'>
+                      <a href="" id="truyen">
+                          <img src={demenphieuluuky} alt="Dế mèn phiêu lưu kí"/>
+                      </a>
+                      </div>
+                      <div className='product-info'> 
+                            <a href='' className='product-name'>Dế mèn phiêu lưu kí</a>
+                            <div className='product-author'>Tô Hoài</div>
+                            <div className='product-price'> 102.000 đ</div>
+                      </div>
+                      <button className='btn' type='submit'> Chọn mua </button>
+                  </div>
+              </li>
+              <li>
+                  <div className='product-item'>
+                      <div className='product-top'>
+                      <a href="" id="truyen">
+                          <img src={nhungngaythoau} alt="Những ngày thơ ấu"/>
+                      </a>
+                      </div>
+                      <div className='product-info'> 
+                            <a href='' className='product-name'>Những ngày thơ ấu</a>
+                            <div className='product-author'>Nguyên Hồng</div>
+                            <div className='product-price'> 71.000 đ</div>
+                      </div>
+                      <button className='btn' type='submit'> Chọn mua </button>
+                  </div>
+              </li>
+              <li>
+                  <div className='product-item'>
+                      <div className='product-top'>
+                      <a href="" id="truyen">
+                          <img src={lamdi} alt="Làm đĩ"/>
+                      </a>
+                      </div>
+                      <div className='product-info'> 
+                            <a href='' className='product-name'>Làm đĩ</a>
+                            <div className='product-author'>Vũ Trọng Phụng</div>
+                            <div className='product-price'> 87.000 đ</div>
                       </div>
                       <button className='btn' type='submit'> Chọn mua </button>
                   </div>
